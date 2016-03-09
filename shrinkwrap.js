0d278ebf7fc5c9f3b3ba2d864b45f04d6a3bf654
michaeld <michaeld@liveperson.com>
2016-03-09 11:34:27 -0800

    changes somefile

--- shrinkwrap.js
+++ shrinkwrap.js
@@ -12,35 +12,38 @@ git.then(function () {
     })
     .tags(function(err, tags) {
         latestTagName = tags.all[0];
-    })
-    .checkout("v0.0.0", function (err) {
-        console.log("Checkout tag done:"+latestTagName);
-    })
-    .then(function () {
-        console.log("did some shit");
-        //npm.load({
-        //    prefix: process.cwd(),
-        //    dev: true,
-        //    loglevel: 'error'
-        //}, function (er) {
-        //    if (er) {
-        //        throw er;
-        //    }
-        //    npm.commands.shrinkwrap({}, true, function (er) {
-        //        if (er) {
-        //            throw er;
-        //        }
-        //        // command succeeded, and data might have some info
-        //        grunt.log("Finished shrinkwrap");
-        //    });
-        //});
-    })
-    //.add(["somefile.js"])
-    //.commit("adding somefile.json")
-    //.push('origin', latestTagName)
-    .then(function () {
-        //grunt.log("error while shrinkwrapping: " + err.message);
-        console.log("done");
+        git.then(function() {
+                console.log("Latest tag name:"+latestTagName);
+            })
+            .checkoutBranch(latestTagName, latestTagName, function (err) {
+                console.log("Checkout tag done:"+latestTagName);
+            })
+            .then(function () {
+                console.log("did some shit");
+                //npm.load({
+                //    prefix: process.cwd(),
+                //    dev: true,
+                //    loglevel: 'error'
+                //}, function (er) {
+                //    if (er) {
+                //        throw er;
+                //    }
+                //    npm.commands.shrinkwrap({}, true, function (er) {
+                //        if (er) {
+                //            throw er;
+                //        }
+                //        // command succeeded, and data might have some info
+                //        grunt.log("Finished shrinkwrap");
+                //    });
+                //});
+            })
+            //.add(["somefile.js"])
+            //.commit("adding somefile.json")
+            .push('origin', latestTagName)
+            .then(function () {
+                //grunt.log("error while shrinkwrapping: " + err.message);
+                console.log("done");
+            });
     });
 
 //module.export = {
