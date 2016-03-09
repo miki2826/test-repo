49358b304036963da72dc449a98377c5611cef2a
michaeld <michaeld@liveperson.com>
2016-03-09 11:24:18 -0800

    changes somefile

--- shrinkwrap.js
+++ shrinkwrap.js
@@ -4,49 +4,6 @@ var npm = require("npm");
 
 var simple = require("simple-git");
 
-function doShrinkWrap(grunt) {
-    var latestTagName;
-    var git = simple();
-    git.then(function () {
-            grunt.log("checking out latest tag");
-        })
-        .tags(function(err, tags) {
-            latestTagName = tags.latest;
-        })
-        .checkoutLatestTag(function () {
-            grunt.log("Checkout tag done");
-        })
-        .then(function () {
-            npm.load({
-                prefix: process.cwd(),
-                dev: true,
-                loglevel: 'error'
-            }, function (er) {
-                if (er) {
-                    throw er;
-                }
-                npm.commands.shrinkwrap({}, true, function (er) {
-                    if (er) {
-                        throw er;
-                    }
-                    // command succeeded, and data might have some info
-                    grunt.log("Finished shrinkwrap");
-                });
-            });
-        })
-        .add(["npm-shrinkwrap.json"])
-        .commit("adding npm-shrinkwrap.json")
-        .push('origin', latestTagName)
-        .then(function () {
-            //grunt.log("error while shrinkwrapping: " + err.message);
-            grunt.log("done");
-        });
-}
-////TODO: test - remove
-//doShrinkWrap({
-//    log: console.log,
-//    fatal: console.log
-//});
 var latestTagName;
 var git = simple();
 
@@ -54,10 +11,10 @@ git.then(function () {
         console.log("checking out latest tag");
     })
     .tags(function(err, tags) {
-        latestTagName = tags.latest;
+        latestTagName = tags.all[0];
     })
-    .checkoutLatestTag(function () {
-        console.log("Checkout tag done");
+    .checkout(latestTagName, function () {
+        console.log("Checkout tag done:"+latestTagName);
     })
     .then(function () {
         console.log("did some shit");
@@ -78,9 +35,9 @@ git.then(function () {
         //    });
         //});
     })
-    .add(["somefile.js"])
-    .commit("adding somefile.json")
-    .push('origin', latestTagName)
+    //.add(["somefile.js"])
+    //.commit("adding somefile.json")
+    //.push('origin', latestTagName)
     .then(function () {
         //grunt.log("error while shrinkwrapping: " + err.message);
         console.log("done");
